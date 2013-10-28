e3e61fb93c6416fffef2b0ea155f4f4e1c0be735
iChun <ohai.iChun@gmail.com>
2013-10-29 04:04:38 +0800

    More base work done.

--- com/mobvsmob/MobVsMob.java
+++ com/mobvsmob/MobVsMob.java
@@ -11,7 +11,11 @@ import java.util.logging.Logger;
 
 import net.minecraftforge.common.Property;
 
+import com.mobvsmob.client.core.PacketHandlerClient;
 import com.mobvsmob.common.core.CommonProxy;
+import com.mobvsmob.common.core.ConnectionHandler;
+import com.mobvsmob.common.core.MapPacketHandler;
+import com.mobvsmob.common.core.PacketHandlerServer;
 
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
@@ -21,7 +25,9 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.FMLNetworkHandler;
 import cpw.mods.fml.common.network.NetworkMod;
+import cpw.mods.fml.common.network.NetworkMod.SidedPacketHandler;
 import cpw.mods.fml.common.network.NetworkModHandler;
+import cpw.mods.fml.common.registry.GameRegistry;
 
 @Mod(modid = "MobVsMob", name = "MobVsMob",
 			version = MobVsMob.version,
@@ -29,7 +35,11 @@ import cpw.mods.fml.common.network.NetworkModHandler;
 				)
 @NetworkMod(clientSideRequired = true,
 			serverSideRequired = false,
-			versionBounds = "[" + iChunUtil.versionMC + "." + MobVsMob.versionMajor + ".0" + "," + iChunUtil.versionMC + "." + (MobVsMob.versionMajor + 1) + ".0" + ")"
+			versionBounds = "[" + iChunUtil.versionMC + "." + MobVsMob.versionMajor + ".0" + "," + iChunUtil.versionMC + "." + (MobVsMob.versionMajor + 1) + ".0" + ")",
+			connectionHandler = ConnectionHandler.class,
+			tinyPacketHandler = MapPacketHandler.class,
+			clientPacketHandlerSpec = @SidedPacketHandler(channels = { "MobVsMob" }, packetHandler = PacketHandlerClient.class ),
+			serverPacketHandlerSpec = @SidedPacketHandler(channels = { "MobVsMob" }, packetHandler = PacketHandlerServer.class )
 				)
 public class MobVsMob 
 	implements IConfigUser
@@ -60,6 +70,7 @@ public class MobVsMob
 	@EventHandler
 	public void load(FMLInitializationEvent event)
 	{
+		GameRegistry.registerPlayerTracker(new ConnectionHandler());
 		proxy.initMod();
 	}
 
--- com/mobvsmob/common/core/SessionState.java
+++ com/mobvsmob/common/core/SessionState.java
@@ -3,5 +3,9 @@ package com.mobvsmob.common.core;
 //Session specific states synched between the client and server goes here.
 public final class SessionState 
 {
-
+	
+	
+	public static void resetSession()
+	{
+	}
 }
