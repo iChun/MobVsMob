bd735fb450f832ed61b6a787842b9ff09f0f5254
Kihira <zoe.kihira.lee@gmail.com>
2013-10-28 21:01:07 +0000

    Begin work on commands

---
+++ com/mobvsmob/common/commands/CommandMap.java
@@ -0,0 +1,21 @@
+package com.mobvsmob.common.commands;
+
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.ICommandSender;
+
+public class CommandMap extends CommandBase {
+	@Override
+	public String getCommandName() {
+		return "map";
+	}
+
+	@Override
+	public String getCommandUsage(ICommandSender icommandsender) {
+		return null;
+	}
+
+	@Override
+	public void processCommand(ICommandSender icommandsender, String[] astring) {
+
+	}
+}
